2a2df4a6dfdb3964700fa19486114ee6d2e9f54a
Arjun <arjun.narayanan91@gmail.com>
2020-11-17 14:31:12 -0800

    Removed unnecessary type assertions from lagrange_polynomials.jl

--- src/lagrange_polynomials.jl
+++ src/lagrange_polynomials.jl
@@ -1,25 +1,21 @@
-function vandermonde_matrix(points::V) where {V<:AbstractVector}
+function vandermonde_matrix(points)
     N = length(points)
-    m = hcat([points.^i for i = 0:N-1]...)
+    m = hcat([points .^ i for i = 0:N-1]...)
     return m
 end
 
-function lagrange_polynomial_coefficients(points::V) where {V<:AbstractVector}
+function lagrange_polynomial_coefficients(points)
     matrix = vandermonde_matrix(points)
     return inv(matrix)
 end
 
-function polynomial_from_coefficients(x::DP.PolyVar,coeffs::V) where {V<:AbstractVector}
-
+function polynomial_from_coefficients(x, coeffs)
     N = length(coeffs)
-    return sum([coeffs[i+1]*x^i for i = 0:N-1])
-
+    return sum([coeffs[i+1] * x^i for i = 0:N-1])
 end
 
-function lagrange_polynomials(x::DP.PolyVar,interpolation_points::V) where {V<:AbstractVector}
-
+function lagrange_polynomials(x, interpolation_points)
     N = length(interpolation_points)
     coeffs = lagrange_polynomial_coefficients(interpolation_points)
-    return [polynomial_from_coefficients(x,coeffs[:,i]) for i = 1:N]
-
+    return [polynomial_from_coefficients(x, coeffs[:, i]) for i = 1:N]
 end
